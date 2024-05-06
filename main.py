c33f08325d3e476695ee340534a94636779dfd40
G.Getsov <G.Getsov.Dev@gmail.com>
2024-05-06 20:40:16 +0300

    Walking animation

--- main.py
+++ main.py
@@ -4,24 +4,14 @@ import win32gui, win32con, win32api
 from src.states import Idle
 from src.transform import screen, win
 
-background_color = (255,255,255)
-
 # remove background/add tranparency
+background_color = (255,255,255)
 win32gui.SetWindowLong(win.hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(win.hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(win.hwnd, win32api.RGB(*background_color), 0, win32con.LWA_COLORKEY)
-
-image = pygame.image.load("./sprites/risi.png") 
-image_size_in_px = 32
-image = pygame.transform.scale_by(image,win.size.x/image_size_in_px)
 
 win.move(win.pos.x, win.pos.y)
 #set on top
 win32gui.SetWindowPos(win.hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
-
-screen.real.fill(background_color)
-screen.real.blit(image,(0,0))
-
-pygame.display.flip()
 
 from src.states import Idle
 
@@ -41,8 +31,5 @@ while running:
 
   current_state.on_update()
 
-  # screen.fill(background_color)
-  # screen.blit(image,(0,0))
-
   # pygame.display.flip()
-  clock.tick(60)
+  clock.tick(30)
--- src/states.py
+++ src/states.py
@@ -4,17 +4,29 @@ import win32gui, win32con, win32api
 import random
 from typing import TypeVar
 
-from src.transform import  win
+from src.transform import  screen, win
 
-walking_frames = []
-walking_frames.append(pygame.image.load('./sprites/walking_1.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_2.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_3.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_4.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_5.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_6.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_7.png'))
-walking_frames.append(pygame.image.load('./sprites/walking_8.png'))
+background_color = (255,255,255)
+image_size_in_px = 32
+scaling_factor = win.size.x/image_size_in_px
+
+def load_sprite(name: str):
+  return pygame.transform.scale_by(pygame.image.load('./sprites/'+name+'.png'),scaling_factor)
+
+def load_animation(name:str, frames: int):
+  animation = []
+  for i in range(frames):
+    animation.append(load_sprite(name + str(i+1)))
+  return animation
+
+def draw_frame(image: pygame.Surface):
+  screen.real.fill(background_color)
+  screen.real.blit(image,(0,0))
+  pygame.display.flip()
+
+idle = load_sprite("idle")
+
+walking_frames = load_animation('walking_', 8)
 
 TState = TypeVar("TState", bound="State")
 
@@ -27,27 +39,35 @@ class State:
 class Idle(State):
   def __init__(self) -> None:
     self.starting_time = pygame.time.get_ticks()
+    draw_frame(idle)
 
   def should_change_state(self) -> bool:
-    return (pygame.time.get_ticks() - self.starting_time >= 5000)
+    return (pygame.time.get_ticks() - self.starting_time >= 500)
   
   def get_next_state(self):
     return Walking()
-
 
 class Walking(State):
   def __init__(self) -> None:
     self.destination = random.randint(0, win.pos.max_x)
     self.x = win.pos.x 
-    if (self.x > self.destination): self.step = -1 
-    else: self.step = 1
-
+    if (self.x > self.destination): self.step = -3
+    else: self.step = 3
+    self.current_frame = 0
+    
   def on_update(self) -> None:
     self.x += self.step
     win.move(self.x, win.pos.max_y)
+    image = walking_frames[self.current_frame]
+    draw_frame(image)
+    self.current_frame += 1
+    if self.current_frame == len(walking_frames): self.current_frame = 0
+    
 
   def should_change_state(self) -> bool:
-    return self.x == self.destination 
+    # return self.x == self.destination 
+    if self.step > 0: return self.x >= self.destination
+    return self.x <= self.destination
   
   def get_next_state(self):
     win.pos.x = self.x
