7f00f3071fb4ab8418f19234c232800d24efcff2
G.Getsov <G.Getsov.Dev@gmail.com>
2024-05-11 17:51:48 +0300

    Replace file with downloaded

--- src/update.py
+++ src/update.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from os import remove, rename
 
+exe = 'dist/risi.exe'
+tmp = exe + '.tmp' 
 r = requests.get('https://github.com/GGetsov/risi/releases/latest')
 html = r.text
 soup = BeautifulSoup(html, 'html.parser')
 latest_version = soup.find_all("h1")[3].string
 r = requests.get(f'https://github.com/GGetsov/risi/releases/download/v{latest_version}/risi.exe')
-with open('dist/test.exe', 'wb') as file:
+with open(tmp, 'wb') as file:
   file.write(r.content)
+remove(exe)
+rename(tmp, exe)
+
+
